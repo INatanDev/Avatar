d5f7c90a592a0f73b5caa258803395592d14cb67
INatanDev <natan.ferreira21@hotmail.com>
2024-04-24 22:47:59 -0300

    rest api creation

---
+++ src/main/java/application/dto/Customer.java
@@ -0,0 +1,11 @@
+package application.dto;
+
+import domain.models.ProfilePhoto;
+
+import java.util.List;
+
+public record Customer(String customerId, List<String> photos) {
+    public static Customer fromDomain(domain.models.Customer domain) {
+        return new Customer(domain.id(), domain.profilesPhotos().stream().map(ProfilePhoto::generatedPhoto).toList());
+    }
+}
